c736f69855cc1cddefd9d0ed23706b40530a93a3
dallaszkorben <dallaszkorben@gmail.com>
2018-04-11 17:31:59 +0200

    #10 - Print out the status of the result

--- console.py
+++ console.py
@@ -5,13 +5,19 @@ from prala import Record
 COLOR_DEFAULT="\033[00m"
 COLOR_QUESTION="\033[1;37m"
 COLOR_INPUT="\033[1;34m"
-COLOR_GOOD_ANSWER_RIGHT="\033[0;32m"
+COLOR_RESULT_STATUS_WRONG="\033[0;31m"
+COLOR_RESULT_STATUS_RIGHT="\033[0;32m"
 COLOR_GOOD_ANSWER_WRONG="\033[0;31m"
+COLOR_GOOD_ANSWER_RIGHT="\033[0;32m"
 COLOR_STAT="\033[1;33m"
 
 POSITION_QUESTION="\033[10;10H"
 POSITION_INPUT="\033[11;10H"
-POSITION_GOOD_ANSWER="\033[12;10H"
+POSITION_RESULT_STATUS="\033[12;10H"
+POSITION_GOOD_ANSWER="\033[16;10H"
+
+STATUS_WRONG="WRONG"
+STATUS_RIGHT="Right"
 
 def clear_console():
     sys.stdout.write("\033[2J")
@@ -27,17 +33,31 @@ def get_input():
     sys.stdout.write(COLOR_INPUT)
     return input()
 
+def out_result_status(status):
+    sys.stdout.write(POSITION_RESULT_STATUS)
+    if status:
+        sys.stdout.write(COLOR_RESULT_STATUS_RIGHT)
+        print(STATUS_RIGHT)
+    else:
+        sys.stdout.write(COLOR_RESULT_STATUS_WRONG)
+        print(STATUS_WRONG)
+
+    sys.stdout.write(COLOR_DEFAULT)
+
 def out_good_answer_right(answer):
     sys.stdout.write(POSITION_GOOD_ANSWER)
     sys.stdout.write(COLOR_GOOD_ANSWER_RIGHT)
     print(answer)
     sys.stdout.write(COLOR_DEFAULT)
+    out_result_status(True)
 
 def out_good_answer_wrong(answer):
     sys.stdout.write(POSITION_GOOD_ANSWER)
     sys.stdout.write(COLOR_GOOD_ANSWER_WRONG)
     print(answer)
     sys.stdout.write(COLOR_DEFAULT)
+    out_result_status(False)
+
 
 myFilteredDictionary=FiteredDictionary("base", 'hungarian', 'swedish') 
 

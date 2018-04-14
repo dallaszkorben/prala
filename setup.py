f88cff3ad24501fecdf241b5ef2a025375e50fa8
dallaszkorben <dallaszkorben@gmail.com>
2018-04-14 11:30:56 +0200

    added missing __init__.py file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
       name='prala',
       version='0.0.11',
       description='Practice Language',
-      long_description=open('README.txt').read(),
+      long_description=open('README.md').read(),
       url='http://github.com/dallaszkorben/prala',
       author='dallaszkorben',
       author_email='dallaszkorben@gmail.com',
@@ -13,14 +13,15 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
       ],
-      packages=find_packages(),
+      packages = find_packages(),
       install_requires=[
-          'pyttsx3','shelve', 'random', 'numpy', 'sys'
+          'pyttsx3', 'numpy',
       ],
       entry_points = {
         'console_scripts': ['pracon=prala.console:main'],
       },
       package_data={
         'prala': ['*.txt'],
-      include_package_data=True,
+      },
+      include_package_data = True,
       zip_safe=False)
